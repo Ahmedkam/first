caed8d6216149e9f3f19090bcdee2a5a702e8d15
Ahmedkam <44031632+Ahmedkam@users.noreply.github.com>
2018-10-10 17:39:16 +0200

    Add files via upload

---
+++ JavaApplication1.java
@@ -0,0 +1,17 @@
+
+package javaapplication1;
+
+
+public class JavaApplication1 {
+
+
+    public static void main(String[] args) {
+        
+        int x=10;
+        int y=20;
+        int z=50;
+        
+        System.out.println(x+y);
+    }
+    
+}
